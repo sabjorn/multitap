7408c3f22c49fbe69c5abef656416b60ec1a1b33
sabjorn <info@sabjorn.net>
2023-12-02 15:11:37 -0800

    everything works for tests except for moving. going to try to use RefCell for the references of Multitap held by ReadHead and WriteHead

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#![no_std]
+//#![no_std]
+use core::cell::UnsafeCell;
 use core::ops::{Index, IndexMut};
 
 pub trait Num: Copy + Send{
@@ -17,175 +18,119 @@ impl Num for i32 {
     }
 }
 
-pub struct ReadHead<T: Num> {
-    buffer : * const [T],
-    size : usize,
+pub struct Multitap<T: Num, const N: usize> {
+    data: UnsafeCell<[T; N]>,
+    writehead_exists: bool,
+}
+
+pub struct ReadHead<'a, T: Num, const N: usize> {
+    buffer: &'a Multitap<T, N>,
     head_position : usize,
 }
 
-unsafe impl<T: Num> Send for ReadHead<T> {}
+pub struct WriteHead<'a, T: Num, const N: usize> {
+    buffer: &'a Multitap<T, N>,
+    head_position : usize,
+}
 
-impl<T: Num> ReadHead<T> {
-    pub fn seek(&mut self, position: usize){
-        self.head_position = position % self.size;
+impl<T: Num, const N: usize> Multitap<T, N> 
+where T: Default {
+    pub fn new() -> Self {
+        Multitap {
+            data: UnsafeCell::new([T::default_value(); N]),
+            writehead_exists: false,
+        }
+    }
+
+    pub fn as_mut(&self) -> &mut [T; N] {
+        unsafe { &mut *self.data.get() }
+    }
+    
+    pub fn as_writehead(&mut self) -> Option<WriteHead<T, N>> {
+        if self.writehead_exists {
+            return None;
+        }
+
+        self.writehead_exists = true;
+        Some(WriteHead {
+            buffer: self,
+            head_position: 0 
+        })
+    }
+
+    pub fn as_readhead(&self, head_position: usize) -> ReadHead<T, N> {
+        ReadHead {
+            buffer: self,
+            head_position: (N - head_position) % N,
+        }
     }
 }
 
-impl<T: Num> Iterator for ReadHead<T> {
+unsafe impl<'a, T: Num, const N: usize> Send for ReadHead<'a, T, N> {}
+
+//impl<'a, T: Num, const N: usize> ReadHead<'a, T, N> {
+//    pub fn seek(&mut self, position: usize){
+//        self.head_position = position % self.size;
+//    }
+//}
+//
+impl<'a, T: Num, const N: usize> Iterator for ReadHead<'a, T, N> 
+where T: Default {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
-        let sample: T;
-        unsafe {
-            sample = (*self.buffer)[self.head_position];
-        }
-        self.head_position = (self.head_position + 1) % (self.size);
+        let sample = self.buffer.as_mut()[self.head_position];
+        self.head_position = (self.head_position + 1) % N;
 
         Some(sample)
     }
 }
 
-impl<T: Num> Index<usize> for ReadHead<T> {
+impl<'a, T: Num, const N: usize> Index<usize> for ReadHead<'a, T, N> 
+where T: Default{
     type Output = T;
     fn index(&self, i: usize) -> &T {
-        let current_position = i % self.size;
-        unsafe {
-            &(*self.buffer)[current_position]
-        }
+        let current_position = (self.head_position + i) % N;
+        &self.buffer.as_mut()[current_position]
     }
 }
 
+unsafe impl<'a, T: Num, const N: usize> Send for WriteHead<'a, T, N> {}
 
-pub struct WriteHead<T: Num, const N: usize> {
-    buffer : [T; N],
-    head_position : usize,
-}
-
-unsafe impl<T: Num, const N: usize> Send for WriteHead<T, N> {}
-
-impl<T: Num, const N: usize> WriteHead<T, N> {
-    pub fn new() -> WriteHead<T, N> {
-        let buffer = [ T::default_value(); N];
-        WriteHead {buffer, head_position: 0}
-    }
-
+impl<'a, T: Num, const N: usize> WriteHead<'a, T, N>
+where T: Default {
     pub fn push(&mut self, element: T) {
-        self.buffer[self.head_position] = element;
+        let buffer = self.buffer.as_mut();
+        buffer[self.head_position] = element;
         self.increment();
     }
     
     pub fn increment(&mut self) {
-        self.head_position = (self.head_position + 1) % self.buffer.len();
+        self.head_position = (self.head_position + 1) % N;
     }
-
-    pub fn seek(&mut self, position: usize){
-        self.head_position = if position > self.buffer.len() { 0 } else { position };
-    }
-
-    pub fn clear(&mut self) where T: Default {
-        self.buffer.fill(T::default_value());
-    }
-    
-    pub fn as_readhead(&self, delay_samples: usize) -> ReadHead<T> {
-        ReadHead {buffer: self.buffer.as_slice(), size: self.buffer.len(), head_position: (self.buffer.len() - delay_samples) % self.buffer.len()}
-    }
+//
+//    pub fn seek(&mut self, position: usize){
+//        self.head_position = if position > self.buffer.len() { 0 } else { position };
+//    }
+//
+//    pub fn clear(&mut self) where T: Default {
+//        self.buffer.fill(T::default_value());
+//    }
 }
 
-
-impl<T: Num, const N: usize> Iterator for WriteHead<T, N> {
-    type Item = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        let sample = self.buffer[self.head_position];
-        self.increment();
-
-        Some(sample)
-    }
-}
-
-impl<T: Num, const N: usize> Index<usize> for WriteHead<T, N> {
+impl<'a, T: Num, const N: usize> Index<usize> for WriteHead<'a, T, N> 
+where T: Default{
     type Output = T;
     fn index(&self, i: usize) -> &T {
-        let current_position = i % self.buffer.len();
-        &self.buffer[current_position]
+        let current_position = (self.head_position + i) % N;
+        &self.buffer.as_mut()[current_position]
     }
 }
 
-impl<T: Num, const N: usize> IndexMut<usize> for WriteHead<T, N> {
+impl<'a, T: Num, const N: usize> IndexMut<usize> for WriteHead<'a, T, N> 
+where T: Default {
     fn index_mut(&mut self, i: usize) -> &mut T {
-        let current_position = i % self.buffer.len();
-        &mut self.buffer[current_position]
-    }
-}
-
-pub struct WriteHeadOwned<T: Num> {
-    buffer : *mut [T],
-    capacity : usize,
-    head_position : usize,
-}
-
-unsafe impl<T: Num> Send for WriteHeadOwned<T> {}
-
-impl<T: Num> WriteHeadOwned<T> {
-    pub fn new(buffer: &mut[T]) -> WriteHeadOwned<T> {
-        let capacity = buffer.len();
-        WriteHeadOwned {buffer, capacity, head_position: 0}
-    }
-
-    pub fn push(&mut self, element: T) {
-        unsafe {
-            (*self.buffer)[self.head_position] = element;
-        }
-        self.increment();
-    }
-    
-    pub fn increment(&mut self) {
-        self.head_position = (self.head_position + 1) % self.capacity;
-    }
-
-    pub fn seek(&mut self, position: usize){
-        self.head_position = if position > self.capacity { 0 } else { position };
-    }
-
-    pub fn clear(&mut self) where T: Default {
-        unsafe {
-            for i in 0..self.capacity {
-                (*self.buffer)[i] = T::default_value();
-            }
-        }
-    }
-    
-    pub fn as_readhead(&self, delay_samples: usize) -> ReadHead<T> {
-        ReadHead {buffer: self.buffer, size: self.capacity, head_position: (self.capacity - delay_samples) % self.capacity}
-    }
-}
-
-
-impl<T: Num> Iterator for WriteHeadOwned<T> {
-    type Item = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        unsafe {
-            let sample = (*self.buffer)[self.head_position];
-            self.increment();
-            Some(sample)
-        }
-    }
-}
-
-impl<T: Num> Index<usize> for WriteHeadOwned<T> {
-    type Output = T;
-    fn index(&self, i: usize) -> &T {
-        let current_position = i % self.capacity;
-        unsafe {
-            &(*self.buffer)[current_position]
-        }
-    }
-}
-
-impl<T: Num> IndexMut<usize> for WriteHeadOwned<T> {
-    fn index_mut(&mut self, i: usize) -> &mut T {
-        let current_position = i % self.capacity;
-        unsafe {
-            &mut (*self.buffer)[current_position]
-        }
+        let current_position = i % N;
+        &mut self.buffer.as_mut()[current_position]
     }
 }
 
@@ -193,47 +138,61 @@ impl<T: Num> IndexMut<usize> for WriteHeadOwned<T> {
 mod tests {
     use super::*;
 
+    //#[test]
+    //pub fn read_head_is_generic() {
+    //    {
+    //        let mut write_head = WriteHead::<i32, 1>::new();
+    //        write_head.push(0);
+    //    }
+    //    {
+    //        let mut write_head = WriteHead::<f32, 1>::new();
+    //        write_head.push(0_f32);
+    //    }
+    //}
+
     #[test]
-    pub fn read_head_is_generic() {
+    pub fn readhead_with_delay_output_equals_write_head() {
+        let mut multitap = Multitap::<f32, 3>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
+
+        writehead.push(1.0);
+        writehead.push(2.0);
+        writehead.push(3.0);
+
         {
-            let mut write_head = WriteHead::<i32, 1>::new();
-            write_head.push(0);
+            let mut readhead = multitap.as_readhead(0);
+            assert_eq!(readhead.next().unwrap(), 1.0);
+            assert_eq!(readhead.next().unwrap(), 2.0);
+            assert_eq!(readhead.next().unwrap(), 3.0);
         }
-        {
-            let mut write_head = WriteHead::<f32, 1>::new();
-            write_head.push(0_f32);
-        }
-    }
-
-    #[test]
-    pub fn read_head_with_delay_output_equals_write_head() {
-        let mut write_head = WriteHead::<i32, 5>::new();
-
-        write_head.push(1);
-       
-        for n in 0..4 {
-            let mut read_head = write_head.as_readhead(n);
-            for j in 0..4 {
-                let val = read_head.next().unwrap();
-                if j == n {
-                    assert_eq!(val, 1)
-                }
-            }
-        }
-    }
-
-    #[test]
-    pub fn multiple_read_head_with_delay_output_equals_write_head() {
-        let mut write_head = WriteHead::<f32, 5>::new();
-
-        write_head.push(1.0);
         
+        {
+            let mut readhead = multitap.as_readhead(1);
+            assert_eq!(readhead.next().unwrap(), 3.0);
+            assert_eq!(readhead.next().unwrap(), 1.0);
+            assert_eq!(readhead.next().unwrap(), 2.0);
+        }
+        
+        {
+            let mut readhead = multitap.as_readhead(2);
+            assert_eq!(readhead.next().unwrap(), 2.0);
+            assert_eq!(readhead.next().unwrap(), 3.0);
+            assert_eq!(readhead.next().unwrap(), 1.0);
+        }
+    }
+
+    #[test]
+    pub fn multiple_readhead_with_delay_output_equals_write_head() {
+        let mut multitap = Multitap::<f32, 5>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
+
+        writehead.push(1.0);
         for n in 0..4 {
-            let mut read_head_1 = write_head.as_readhead(n);
-            let mut read_head_2 = write_head.as_readhead(n+1);
+            let mut readhead_1 = multitap.as_readhead(n);
+            let mut readhead_2 = multitap.as_readhead(n+1);
             for j in 0..4 {
-                let val_1 = read_head_1.next().unwrap();
-                let val_2 = read_head_2.next().unwrap();
+                let val_1 = readhead_1.next().unwrap();
+                let val_2 = readhead_2.next().unwrap();
                 if j == n {
                     assert_eq!(val_1, 1.0)
                 }
@@ -245,173 +204,136 @@ mod tests {
     }
     
     #[test]
-    pub fn read_head_is_circular() {
-        let mut write_head = WriteHead::<f32, 2>::new();
+    pub fn readhead_is_circular() {
+        let mut multitap = Multitap::<f32, 3>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
         
-        write_head.push(1.0);
+        writehead.push(1.0);
 
-        let mut read_head = write_head.as_readhead(0);
+        let mut readhead = multitap.as_readhead(0);
         
-        assert_eq!(read_head.next().unwrap(), 1.0);
-        assert_eq!(read_head.next().unwrap(), 0.0);
-        assert_eq!(read_head.next().unwrap(), 1.0);
+        assert_eq!(readhead.next().unwrap(), 1.0);
+        assert_eq!(readhead.next().unwrap(), 0.0);
+        assert_eq!(readhead.next().unwrap(), 0.0);
+        assert_eq!(readhead.next().unwrap(), 1.0);
     }
 
     #[test]
-    pub fn read_head_index_operator() {
-        let mut write_head = WriteHead::<f32, 5>::new();
+    pub fn readhead_index_operator() {
+        let mut multitap = Multitap::<f32, 3>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
 
-        for n in 0..4 {
-            write_head.push(n as f32);
-        }
+        writehead.push(0.);
+        writehead.push(1.);
+        writehead.push(2.);
 
-        let read_head = write_head.as_readhead(0);
-        for n in 0..4 {
-            assert_eq!(read_head[n], n as f32);
-        }
+        let readhead = multitap.as_readhead(1);
+        assert_eq!(readhead[0], 2.);
+        assert_eq!(readhead[1], 0.);
+        assert_eq!(readhead[2], 1.);
     }
 
     #[test]
-    pub fn read_head_index_operator_is_circular() {
-        let mut write_head = WriteHead::<f32, 2>::new();
+    pub fn readhead_index_operator_is_circular() {
+        let mut multitap = Multitap::<f32, 3>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
 
-        write_head.push(0.0);
-        write_head.push(1.0);
+        writehead.push(0.0);
+        writehead.push(1.0);
+        writehead.push(2.0);
 
-        let read_head = write_head.as_readhead(0);
-        assert_eq!(read_head[0], 0.0);
-        assert_eq!(read_head[1], 1.0);
-        assert_eq!(read_head[2], 0.0);
-        assert_eq!(read_head[3], 1.0);
-        assert_eq!(read_head[4], 0.0);
-    }
-    
-    #[test]
-    pub fn write_head_is_circular() {
-        let mut write_head = WriteHead::<f32, 2>::new();
-        
-        write_head.push(0.0);
-        write_head.push(0.0);
-        write_head.push(1.0); // wraps around
-
-        let mut read_head = write_head.as_readhead(0);
-        
-        assert_eq!(read_head.next().unwrap(), 1.0);
-        assert_eq!(read_head.next().unwrap(), 0.0);
+        let readhead = multitap.as_readhead(1);
+        assert_eq!(readhead[0], 2.0);
+        assert_eq!(readhead[1], 0.0);
+        assert_eq!(readhead[2], 1.0);
+        assert_eq!(readhead[3], 2.0);
+        assert_eq!(readhead[4], 0.0);
+        assert_eq!(readhead[5], 1.0);
+        assert_eq!(readhead[6], 2.0);
     }
     
     #[test]
-    pub fn write_head_index_operator() {
-        let mut write_head = WriteHead::<f32, 5>::new();
+    pub fn writehead_is_circular() {
+        let mut multitap = Multitap::<f32, 2>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
         
-        for n in 0..4 {
-            write_head[n] = n as f32;
-        }
+        writehead.push(1.0);
+        writehead.push(2.0);
+        writehead.push(3.0); // wraps around
 
-        let mut read_head = write_head.as_readhead(0);
-        for n in 0..4 {
-            assert_eq!(read_head.next().unwrap(), n as f32);
-        }
-    }
-
-    #[test]
-    pub fn write_head_index_operator_is_circular() {
-        let mut write_head = WriteHead::<f32, 2>::new();
+        let mut readhead = multitap.as_readhead(0);
         
-        write_head[0] = 0.0;
-        write_head[1] = 1.0;
-        write_head[2] = 2.0;
-        write_head[3] = 3.0;
-
-        let mut read_head = write_head.as_readhead(0);
-        assert_eq!(read_head.next().unwrap(), 2.0);
-        assert_eq!(read_head.next().unwrap(), 3.0);
-    }
-
-    #[test]
-    pub fn moved_write_head_no_longer_valid() {
-        let mut write_head = WriteHead::<f32, 2>::new();
-
-        write_head[0] = 9.0;
-        write_head[1] = 8.0;
-
-        let mut read_head = write_head.as_readhead(0);
-
-        let mut c = move || {
-            write_head[0] = 0.0;
-            write_head[1] = 1.0;
-        };
-        c();
-
-        assert_eq!(read_head.next().unwrap(), 9.0);
-        assert_eq!(read_head.next().unwrap(), 8.0);
-
-    }
-
-    #[test]
-    pub fn write_head_owned_is_circular() {
-        let mut array: [f32; 2] = [0.0; 2];
-        let mut write_head = WriteHeadOwned::<f32>::new(&mut array);
-        
-        write_head.push(0.0);
-        write_head.push(0.0);
-        write_head.push(1.0); // wraps around
-
-        let mut read_head = write_head.as_readhead(0);
-        
-        assert_eq!(read_head.next().unwrap(), 1.0);
-        assert_eq!(read_head.next().unwrap(), 0.0);
+        assert_eq!(readhead.next().unwrap(), 3.0);
+        assert_eq!(readhead.next().unwrap(), 2.0);
     }
     
     #[test]
-    pub fn write_head_owned_index_operator() {
-        let mut array: [f32; 5] = [0.0; 5];
-        let mut write_head = WriteHeadOwned::<f32>::new(&mut array);
+    pub fn writehead_index_operator() {
+        let mut multitap = Multitap::<f32, 5>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
         
         for n in 0..4 {
-            write_head[n] = n as f32;
+            writehead[n] = n as f32;
         }
 
-        let mut read_head = write_head.as_readhead(0);
+        let mut readhead = multitap.as_readhead(0);
         for n in 0..4 {
-            assert_eq!(read_head.next().unwrap(), n as f32);
+            assert_eq!(readhead.next().unwrap(), n as f32);
         }
     }
 
     #[test]
-    pub fn write_head_owned_index_operator_is_circular() {
-        let mut array: [f32; 2] = [0.0; 2];
-        let mut write_head = WriteHeadOwned::<f32>::new(&mut array);
+    pub fn writehead_index_operator_is_circular() {
+        let mut multitap = Multitap::<f32, 2>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
         
-        write_head[0] = 0.0;
-        write_head[1] = 1.0;
-        write_head[2] = 2.0;
-        write_head[3] = 3.0;
+        writehead[0] = 0.0;
+        writehead[1] = 1.0;
+        writehead[2] = 2.0;
+        writehead[3] = 3.0;
 
-        let mut read_head = write_head.as_readhead(0);
-        assert_eq!(read_head.next().unwrap(), 2.0);
-        assert_eq!(read_head.next().unwrap(), 3.0);
+        let mut readhead = multitap.as_readhead(0);
+        assert_eq!(readhead.next().unwrap(), 2.0);
+        assert_eq!(readhead.next().unwrap(), 3.0);
     }
 
-   
+    //#[test]
+    //pub fn moved_writehead_is_valid() {
+    //    let mut multitap = Multitap::<f32, 2>::new();
+    //    let mut writehead = multitap.as_writehead().unwrap();
+
+    //    writehead[0] = 9.0;
+    //    writehead[1] = 8.0;
+
+    //    let mut c = move || {
+    //        writehead[0] = 0.0;
+    //        writehead[1] = 1.0;
+    //    };
+    //    c();
+
+    //    let mut readhead = multitap.as_readhead(0);
+    //    assert_eq!(readhead.next().unwrap(), 0.0);
+    //    assert_eq!(readhead.next().unwrap(), 1.0);
+
+    //}
+
     #[test]
-    pub fn moved_write_head_owned_is_valid() {
-        let mut array: [f32; 2] = [0.0; 2];
-        let mut write_head = WriteHeadOwned::<f32>::new(&mut array);
+    pub fn moved_writehead_is_valid() {
+        let mut multitap = Multitap::<f32, 2>::new();
+        let mut writehead = multitap.as_writehead().unwrap();
 
-        write_head[0] = 9.0;
-        write_head[1] = 8.0;
+        writehead[0] = 9.0;
+        writehead[1] = 8.0;
 
-        let mut read_head = write_head.as_readhead(0);
+        let mut readhead = multitap.as_readhead(0);
 
-        let mut c = move || {
-            write_head[0] = 0.0;
-            write_head[1] = 1.0;
-        };
-        c();
+        (move || {
+            writehead[0] = 0.0;
+            writehead[1] = 1.0;
+        })();
 
-        assert_eq!(read_head.next().unwrap(), 0.0);
-        assert_eq!(read_head.next().unwrap(), 1.0);
+        assert_eq!(readhead.next().unwrap(), 0.0);
+        assert_eq!(readhead.next().unwrap(), 1.0);
 
     }
 }
